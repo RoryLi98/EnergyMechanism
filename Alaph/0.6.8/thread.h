8aab0d499b5b1a07a7307a69767772e71c503184
Link <sherlock-link@hotmail.com>
2021-06-04 22:43:15 +0800

    Untested Old Prototype

---
+++ Alaph/0.6.8/thread.h
@@ -0,0 +1,18 @@
+#ifndef THREAD_H
+#define THREAD_H
+
+#include "header.h"
+
+class Image_Thread
+{
+public:
+    Image_Thread(int fd2car);
+    //        virtual ~Image_Thread();
+    void Image_Thread_read_from_cam();
+    void Image_Thread_energy();
+
+public:
+    int fd2car;
+};
+
+#endif // THREAD_H
