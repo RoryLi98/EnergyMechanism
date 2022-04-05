fd84a613a6b685d54d169077c3303d507fbb55c8
Link <38213500+LinkLiar@users.noreply.github.com>
2022-04-05 23:15:58 +0800

    Archived

--- Alaph/0.7.1(FinalVersion)/main.cpp
+++ Alaph/0.7.1(FinalVersion)/main.cpp
@@ -5,22 +5,22 @@ using namespace cv;
 int main()
 {
 
-    int fd2car = openPort("/dev/RMCWB"); //开串口
+	int fd2car = openPort("/dev/RMCWB"); //开串口
 
-    cout << "fd2car:" << fd2car << endl;
+	cout << "fd2car:" << fd2car << endl;
 
-    if (fd2car != -1)
-    {
-        configurePort(fd2car);
-    }
+	if (fd2car != -1)
+	{
+		configurePort(fd2car);
+	}
 
-    Image_Thread image_task(fd2car);
-    thread t1(&Image_Thread::Image_Thread_read_from_cam, &image_task);
-    thread t2(&Image_Thread::Image_Thread_energy, &image_task);
+	Image_Thread image_task(fd2car);
+	thread t1(&Image_Thread::Image_Thread_read_from_cam, &image_task);
+	thread t2(&Image_Thread::Image_Thread_energy, &image_task);
 
-    t1.join();
-    t2.join();
-    if (fd2car != -1)
-        close(fd2car);
-    return 0;
+	t1.join();
+	t2.join();
+	if (fd2car != -1)
+		close(fd2car);
+	return 0;
 }
